74f003d44f4bb3ed155ee9b2581bab4bb7dbf76c
dlato <daniellalato@gmail.com>
2021-09-10 11:29:29 -0400

    cleaning up code

--- all_trans_interactions.py
+++ all_trans_interactions.py
@@ -7,15 +7,11 @@
 # email: DaniellaLato@GMail.com
 # GitHub: https://github.com/dlato
 #
-# to run: hicproTo3Dflow.py BED_FILE MATRIX_FILE OUTPUT_PATH RESOLUTION
+# to run: hicproTo3Dflow.py BED_FILE MATRIX_FILE > OUTPUT_PATH
 # input:
 #         - bed file (full path)
-#         - matrix file (full path)
-#         - output path
-#         - resolution
-# output: two files:
-#         - trans.RESOLUTION_iced.sorted.txt
-#         - cis.RESOLUTION_iced.sorted.txt
+# standard output:
+#         - tab separated file with columns: chrA startA endA chrB startB endB
 #
 #####################################
 import sys, re
@@ -29,30 +25,12 @@ import glob #importing glob which helps read files
 
 # read in files
 bed = sys.argv[1]
-#mat = sys.argv[2]
-#out = sys.argv[3]
-#res = sys.argv[4]
-##opening files to write to
-#out_path = out
-#out_cis = out + "/cis." + res + "_iced.sorted.txt"
-#out_trans = out + "/trans." + res + "_iced.sorted.txt"
-#out_fcis = open(out_cis,"w+")
-#out_ftrans = open(out_trans,"w+")
-
 #bed file
 chrn = []
 pos = []
 beddf = pd.read_csv(bed, delimiter='\t', names=['chrA', 'st', 'end'])
-print(beddf.head())
-print(beddf.tail())
-
 uchrs = beddf.chrA.unique()
-print(uchrs)
 ldf = len(beddf)
-print(ldf)
-print(beddf.chrA[0])
-print(beddf.chrA[1])
-
 #vector with starts of each chrom
 for A in range(0,ldf):
     if A+1 != ldf:
@@ -61,7 +39,6 @@ for A in range(0,ldf):
            chrn.append(beddf.chrA[A+1])
 
 c = 0
-#print(beddf.chrA[pos[0]:])
 #loop through each row and print all interactions with that row
 lastchr = beddf.chrA[0]
 for A in range(0,ldf):
