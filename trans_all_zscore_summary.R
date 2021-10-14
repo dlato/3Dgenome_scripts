107d42cbda40805e85512425ebe17da048254b12
dlato <daniellalato@gmail.com>
2021-10-14 12:24:22 -0400

    CL mod

--- trans_all_zscore_summary.R
+++ trans_all_zscore_summary.R
@@ -70,16 +70,16 @@ theme_set(theme_bw() + theme(strip.background =element_rect(fill="#e7e5e2")) +
 
 print("#read in files")
 #interaction data
-zdat_file <- "test_1vsAll_dat.txt"
-pdat_file <- "test_1vsAll_pvalues.txt"
-allinters_file <- "all_trans_interactions_1Mb.txt"
-germlayer_file <- "germlayer_info.txt"
-gl_df <- read.table("germlayer_info.txt",sep = "\t", header = TRUE)
-library(harrypotter)
-#Atype <- "1_vs_All"
-#dat <- read.table("23Jul21.primary.trans.1MB.zscores.txt", header = TRUE)
-#dat <- read.table("23Jul21.primary.trans.1MB.zscores.pairwise.txt", header = TRUE)
-#dat <- read.table(dat_file, header = TRUE)
+#zdat_file <- "test_1vsAll_dat.txt"
+#pdat_file <- "test_1vsAll_pvalues.txt"
+#allinters_file <- "all_trans_interactions_1Mb.txt"
+#germlayer_file <- "germlayer_info.txt"
+#gl_df <- read.table("germlayer_info.txt",sep = "\t", header = TRUE)
+#library(harrypotter)
+##Atype <- "1_vs_All"
+##dat <- read.table("23Jul21.primary.trans.1MB.zscores.txt", header = TRUE)
+##dat <- read.table("23Jul21.primary.trans.1MB.zscores.pairwise.txt", header = TRUE)
+##dat <- read.table(dat_file, header = TRUE)
 zdat <- read.table(zdat_file, header = TRUE)
 pdat <- read.table(pdat_file, header = TRUE)
 print("reading all inters")
