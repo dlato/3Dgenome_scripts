5a8a2469e63fde03be9ee9851c4cf24706850ce7
dlato <daniellalato@gmail.com>
2021-11-09 08:32:31 -0500

    removed germ layer stats test for now

--- trans_all_zscore_summary.R
+++ trans_all_zscore_summary.R
@@ -503,56 +503,56 @@ p
 dev.off()
 #################
 #################
-print("#test between means of germ layers")
-print("#### ALL DATA ####")
-print("#Test each group for normality")
-print("sig = reject normality null")
-#chrClass_dat$chrClass <- as.factor(chrClass_dat$chrClass)
-r_dat3 %>%
-  group_by(germL) %>%
-  summarise(W = shapiro.test(zscore)$statistic,
-            p.value = shapiro.test(zscore)$p.value)
-print("#Perform the Kruskal-Wallis test")
-print("sig = mean is diff btwn groups")
-kruskal.test(zscore ~ germL, data=r_dat3)
-print("# check which groups have sig diff")
-print("# perform pairwise wilcoxon test with FDR (Benjamini-Hochberg) correction")
-pairwise.wilcox.test(r_dat3$zscore, r_dat3$germL,
-                     p.adjust.method = "BH")
-print("#### SIG DATA ####")
-print("#Test each group for normality")
-print("sig = reject normality null")
-#chrClass_dat$chrClass <- as.factor(chrClass_dat$chrClass)
-sig_rdat <- r_dat3 %>%
-  filter(pvalue <= 0.05) 
-sig_rdat %>%
-  group_by(germL) %>%
-  summarise(W = shapiro.test(zscore)$statistic,
-            p.value = shapiro.test(zscore)$p.value)
-print("#Perform the Kruskal-Wallis test")
-print("sig = mean is diff btwn groups")
-kruskal.test(zscore ~ germL, data=sig_rdat)
-print("# check which groups have sig diff")
-print("# perform pairwise wilcoxon test with FDR (Benjamini-Hochberg) correction")
-pairwise.wilcox.test(sig_rdat$zscore, sig_rdat$germL,
-                     p.adjust.method = "BH")
-print("#### NON-SIG DATA ####")
-print("#Test each group for normality")
-print("sig = reject normality null")
-#chrClass_dat$chrClass <- as.factor(chrClass_dat$chrClass)
-sig_rdat <- r_dat3 %>%
-  filter(pvalue >= 0.05) 
-sig_rdat %>%
-  group_by(germL) %>%
-  summarise(W = shapiro.test(zscore)$statistic,
-            p.value = shapiro.test(zscore)$p.value)
-print("#Perform the Kruskal-Wallis test")
-print("sig = mean is diff btwn groups")
-kruskal.test(zscore ~ germL, data=sig_rdat)
-print("# check which groups have sig diff")
-print("# perform pairwise wilcoxon test with FDR (Benjamini-Hochberg) correction")
-pairwise.wilcox.test(sig_rdat$zscore, sig_rdat$germL,
-                     p.adjust.method = "BH")
+#print("#test between means of germ layers")
+#print("#### ALL DATA ####")
+#print("#Test each group for normality")
+#print("sig = reject normality null")
+##chrClass_dat$chrClass <- as.factor(chrClass_dat$chrClass)
+#r_dat3 %>%
+#  group_by(germL) %>%
+#  summarise(W = shapiro.test(zscore)$statistic,
+#            p.value = shapiro.test(zscore)$p.value)
+#print("#Perform the Kruskal-Wallis test")
+#print("sig = mean is diff btwn groups")
+#kruskal.test(zscore ~ germL, data=r_dat3)
+#print("# check which groups have sig diff")
+#print("# perform pairwise wilcoxon test with FDR (Benjamini-Hochberg) correction")
+#pairwise.wilcox.test(r_dat3$zscore, r_dat3$germL,
+#                     p.adjust.method = "BH")
+#print("#### SIG DATA ####")
+#print("#Test each group for normality")
+#print("sig = reject normality null")
+##chrClass_dat$chrClass <- as.factor(chrClass_dat$chrClass)
+#sig_rdat <- r_dat3 %>%
+#  filter(pvalue <= 0.05) 
+#sig_rdat %>%
+#  group_by(germL) %>%
+#  summarise(W = shapiro.test(zscore)$statistic,
+#            p.value = shapiro.test(zscore)$p.value)
+#print("#Perform the Kruskal-Wallis test")
+#print("sig = mean is diff btwn groups")
+#kruskal.test(zscore ~ germL, data=sig_rdat)
+#print("# check which groups have sig diff")
+#print("# perform pairwise wilcoxon test with FDR (Benjamini-Hochberg) correction")
+#pairwise.wilcox.test(sig_rdat$zscore, sig_rdat$germL,
+#                     p.adjust.method = "BH")
+#print("#### NON-SIG DATA ####")
+#print("#Test each group for normality")
+#print("sig = reject normality null")
+##chrClass_dat$chrClass <- as.factor(chrClass_dat$chrClass)
+#sig_rdat <- r_dat3 %>%
+#  filter(pvalue >= 0.05) 
+#sig_rdat %>%
+#  group_by(germL) %>%
+#  summarise(W = shapiro.test(zscore)$statistic,
+#            p.value = shapiro.test(zscore)$p.value)
+#print("#Perform the Kruskal-Wallis test")
+#print("sig = mean is diff btwn groups")
+#kruskal.test(zscore ~ germL, data=sig_rdat)
+#print("# check which groups have sig diff")
+#print("# perform pairwise wilcoxon test with FDR (Benjamini-Hochberg) correction")
+#pairwise.wilcox.test(sig_rdat$zscore, sig_rdat$germL,
+#                     p.adjust.method = "BH")
 
 ##################
 ##common interactions z-scores broken down by chrom class
