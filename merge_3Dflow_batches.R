6add1cf697d1604e1820025eec29d1f5e24ed76a
Daniella Lato, PhD <daniellalato@gmail.com>
2021-11-19 16:57:10 -0500

    merging 3Dflow batches, need to test saving

---
+++ merge_3Dflow_batches.R
@@ -0,0 +1,50 @@
+########################################
+#merging zscore or pvalue df output from 3Dflow
+######
+# Developer: Daniella F. Lato
+#            email:  daniellalato@gmail.com
+#            github: https://github.com/dlato
+######
+# arguments: 3Dflow z-score output data (tsv) ** make sure it is ALL zscores! (not just sig)
+#            3Dflow p-value output data (tsv) ** make sure it is ALL pvalues! (not just sig)
+#            germlayer df (tsv)
+#            all interactions df (tsv)
+########################################
+
+options(echo=F)
+args <- commandArgs(trailingOnly = TRUE)
+zdat_file <- args[1]
+pdat_file <- args[2]
+germlayer_file <- args[3]
+allinters_file <- args[4]
+#Atype <- args[4]
+
+##########
+#library(dplyr)
+#library(tidyr)
+library(tidyverse)
+#library(GenomicRanges)
+library(ggplot2)
+library(ggforce)#for ridgeline
+library(ggridges)#for ridgeline
+library(ggbiplot, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
+library(circlize, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.2")#for circos
+library(gtools)#for sorting alphanumeric
+library(devtools)#for PCA
+library(factoextra,lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
+library(harrypotter, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")
+#install_github("vqv/ggbiplot")
+##remotes::install_github("R-CoderDotCom/ridgeline@main")
+#library(ridgeline)
+##########
+
+#########################################################################
+
+
+print("#read in files")
+args <- list("testFileName.txt", "test_df1.txt","test_df2.txt")
+dfargs <- args[-1]
+#list of dfs
+l_dfs <- lapply(dfargs, read.table,header =TRUE)
+mdat <- reduce(l_dfs, full_join, by = "ID")
+write.table(mdat, file = args[[1]], sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
