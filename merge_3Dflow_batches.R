6c4e0416905c664a0a6398bd9cb4afeed19a75f8
dlato <daniellalato@gmail.com>
2021-11-22 14:46:19 -0500

    keep col names

--- merge_3Dflow_batches.R
+++ merge_3Dflow_batches.R
@@ -13,22 +13,7 @@ options(echo=F)
 args <- commandArgs(trailingOnly = TRUE)
 
 ##########
-#library(dplyr)
-#library(tidyr)
 library(tidyverse)
-##library(GenomicRanges)
-#library(ggplot2)
-#library(ggforce)#for ridgeline
-#library(ggridges)#for ridgeline
-#library(ggbiplot, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
-#library(circlize, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.2")#for circos
-#library(gtools)#for sorting alphanumeric
-#library(devtools)#for PCA
-#library(factoextra,lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
-#library(harrypotter, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")
-##install_github("vqv/ggbiplot")
-###remotes::install_github("R-CoderDotCom/ridgeline@main")
-##library(ridgeline)
 ##########
 
 #########################################################################
@@ -40,4 +25,4 @@ dfargs <- args[-1]
 #list of dfs
 l_dfs <- lapply(dfargs, read.table,header =TRUE)
 mdat <- reduce(l_dfs, full_join, by = "ID")
-write.table(mdat, file = args[[1]], sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(mdat, file = args[[1]], sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
