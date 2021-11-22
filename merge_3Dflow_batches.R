e1c3a865503b4e0e2fc63039719111a3896abd49
Daniella Lato <latodf@mcmaster.ca>
2021-11-22 10:49:41 -0500

    cleaned

--- merge_3Dflow_batches.R
+++ merge_3Dflow_batches.R
@@ -5,44 +5,37 @@
 #            email:  daniellalato@gmail.com
 #            github: https://github.com/dlato
 ######
-# arguments: 3Dflow z-score output data (tsv) ** make sure it is ALL zscores! (not just sig)
-#            3Dflow p-value output data (tsv) ** make sure it is ALL pvalues! (not just sig)
-#            germlayer df (tsv)
-#            all interactions df (tsv)
+# arguments: full path and file name for output merged dataframe
+#            all dataframes to merge with full path, each must be separated by space. Can merge unlimited dfs
 ########################################
 
 options(echo=F)
 args <- commandArgs(trailingOnly = TRUE)
-zdat_file <- args[1]
-pdat_file <- args[2]
-germlayer_file <- args[3]
-allinters_file <- args[4]
-#Atype <- args[4]
 
 ##########
 #library(dplyr)
 #library(tidyr)
 library(tidyverse)
-#library(GenomicRanges)
-library(ggplot2)
-library(ggforce)#for ridgeline
-library(ggridges)#for ridgeline
-library(ggbiplot, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
-library(circlize, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.2")#for circos
-library(gtools)#for sorting alphanumeric
-library(devtools)#for PCA
-library(factoextra,lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
-library(harrypotter, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")
-#install_github("vqv/ggbiplot")
-##remotes::install_github("R-CoderDotCom/ridgeline@main")
-#library(ridgeline)
+##library(GenomicRanges)
+#library(ggplot2)
+#library(ggforce)#for ridgeline
+#library(ggridges)#for ridgeline
+#library(ggbiplot, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
+#library(circlize, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.2")#for circos
+#library(gtools)#for sorting alphanumeric
+#library(devtools)#for PCA
+#library(factoextra,lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")#for PCA
+#library(harrypotter, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3")
+##install_github("vqv/ggbiplot")
+###remotes::install_github("R-CoderDotCom/ridgeline@main")
+##library(ridgeline)
 ##########
 
 #########################################################################
 
 
 print("#read in files")
-args <- list("testFileName.txt", "test_df1.txt","test_df2.txt")
+#args <- list("testFileName.txt", "test_df1.txt","test_df2.txt")
 dfargs <- args[-1]
 #list of dfs
 l_dfs <- lapply(dfargs, read.table,header =TRUE)
