7c79a393472077f0367b1822b0ebf9f479a7b7ca
Daniella Lato, PhD <daniellalato@gmail.com>
2021-11-26 13:24:10 -0500

    sum total number of cells with interaction as common

---
+++ trans_common_sig_interactions_window_analysis.R
@@ -0,0 +1,168 @@
+########################################
+#validating known trans-chromosomal interactions
+######
+# Developer: Daniella F. Lato
+#            email:  daniellalato@gmail.com
+#            github: https://github.com/dlato
+######
+# arguments: 3Dflow output data (tsv)
+#            germlayer df (tsv)
+#            all interactions file (tsv)
+#            tissue/system df (tsv)
+#            bin size (bp)
+########################################
+
+options(echo=F)
+args <- commandArgs(trailingOnly = TRUE)
+dat_file <- args[1]
+germlayer_file <- args[2]
+allinters_file <- args[3]
+tissue_file <- args[4]
+bin_size <- args[5]
+#Atype <- args[4]
+
+##########
+library(tidyr)
+library(dplyr)
+library(GenomicRanges)
+library(ggplot2)
+library(ggforce)#for ridgeline
+library(ggridges)#for ridgeline
+library(ggbiplot)#for PCA
+library(devtools)#for PCA
+.libPaths("/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.2")
+library(circlize,lib = "/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.2/") # for circos
+library(regioneR,lib = "/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.2/")#for permutation
+library(factoextra, lib = "/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3/")#for PCA
+library(harrypotter, lib="/hpf/largeprojects/pmaass/programs/Rlib/R.4.0.3") #for colours
+library(regioneR)
+#install_github("vqv/ggbiplot")
+##remotes::install_github("R-CoderDotCom/ridgeline@main")
+#library(ridgeline)
+##########
+
+#########################################################################
+#set graph theme
+theme_set(theme_bw() + theme(strip.background =element_rect(fill="#e7e5e2")) +
+            #change size of facet header text
+            theme(strip.text = element_text(size =10.49)) +
+            theme(plot.title = element_text(hjust = 0.5, size = 18),
+                  panel.background = element_rect(fill = "white", colour = NA),
+                  panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  panel.spacing = unit(0.25, "lines"),
+                  axis.text=element_text(size=18),
+                  axis.title = element_text(size = 18),
+                  #plot margins
+                  #plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"),
+                  #for second legend on y-axis
+                  axis.text.y.right = element_text(size=18),
+                  #legend.title = element_blank(),
+                  legend.title = element_text(size = 16),
+                  legend.text = element_text(size = 18),
+                  #change the colour of facet label background
+                  strip.background = element_rect(fill = "#E6E1EA"),
+                  #remove space between facet
+                  panel.spacing.x=unit(0, "lines"),
+                  #                  legend.key = element_blank(),
+                  legend.background=element_blank(),
+                  #legend background
+                  legend.key = element_rect(fill = NA),
+                  #                  legend.position="none")
+                  legend.position="top")
+)
+#########################################################################
+
+
+print("#read in files")
+##interaction data
+Atype <- "1_vs_All"
+tissue_file <- "tissue_system_info.txt"
+dat_file <- "test_pairwise_dat.txt"
+allinters_file <- "all_trans_interactions_1Mb.txt"
+germlayer_file <- "germlayer_info.txt"
+bin_size <- 1000000
+library(factoextra)#for PCA
+library(harrypotter) #for colours
+
+allinters <- read.table(allinters_file, header = FALSE)
+colnames(allinters) <- c("chrA", "startA", "endA", "chrB", "startB", "endB")
+head(allinters)
+dat <- read.table(dat_file, header = TRUE)
+print("summary of ALL sig zscores per cell type")
+summary(dat)
+dat$ID <- as.character(dat$ID)
+print("#read in germlayer info file")
+print(germlayer_file)
+gl_df <- read.table(germlayer_file, header = TRUE, sep="\t")
+print("colnames")
+colnames(gl_df) <- c("cell","germLayer")
+summary(gl_df)
+#order of germlayer
+gl_ord <- c("ectoderm", "mesoderm", "endoderm", "bipotent", "ectoderm/mesoderm")
+#gl_colours <- c("#0D3B66","#F4D35E","#F95738","#66999B","#EE964B")
+#darker shades
+gl_colours <- c("#071F36","#F2CB40","#ED2E07","#517A7B","#EA7E1F")
+
+#tissue info
+ts_df <- read.table(tissue_file,sep = "\t", header = TRUE)
+head(ts_df)
+
+#remove interactions involving x and y chrs
+#dat <- dat[grep("chrY", df$ID, invert=TRUE), ]
+#dat2 <- dat[grep("chrY", df$ID), ]
+#dat <- dat[grep("chrX", df$ID, invert=TRUE), ]
+
+#select only rows with NO NAs in any cell type
+print("all chrom pairs involved in ALL SIG interactions")
+#split ID col
+colnm <- c("chrA", "st1", "end1","chrB","st2","end2")
+tmpD <- dat
+tmpD$ID <- sub("B", "\\.B", as.character(tmpD$ID))
+tmpD <- tmpD %>% separate(ID, sep = "\\.", into = colnm, remove = FALSE)
+#remove A and B from chrom names
+tmpD$chrA <- gsub("A", "", tmpD$chrA)
+tmpD$chrB <- gsub("B", "", tmpD$chrB)
+tmpD$chrPair <- paste(tmpD$chrA,tmpD$chrB)
+unique(tmpD$chrPair)
+
+#remove NAs and therefore only look at common inters
+df <- dat
+print("Total number of common interactions")
+length(df$ID)
+print("Total number of ALL possible interactions")
+length(allinters$chrA)
+print("percent of ALL common interactions across genome: all possible interactions")
+(length(df$ID)/length(allinters$chrA)) *100
+print("percent of ALL common interactions across genome: interactions in dataset")
+(length(df$ID)/length(dat$ID)) *100
+
+#split ID col
+colnm <- c("chrA", "st1", "end1","chrB","st2","end2")
+df$ID <- sub("B", "\\.B", as.character(df$ID))
+dat2 <- df %>% separate(ID, sep = "\\.", into = colnm, remove = FALSE)
+#remove A and B from chrom names
+dat2$chrA <- gsub("A", "", dat2$chrA)
+dat2$chrB <- gsub("B", "", dat2$chrB)
+
+print("all chroms involved in common interactions")
+Achrs <- unique(c(dat2$chrA, dat2$chrB))
+Achrs
+print("all chrom pairs involved in common interactions")
+tmpD <- dat2
+tmpD$chrPair <- paste(dat2$chrA,dat2$chrB)
+unique(tmpD$chrPair)
+
+print("#########################")
+print("# calculating number of cells that have the interaction as common")
+print("#########################")
+tot_cells_per_inter <- dat2
+rownames(tot_cells_per_inter) <- tot_cells_per_inter$ID
+tot_cells_per_inter <- tot_cells_per_inter %>% select(-ID,-chrA,-st1,-end1,-chrB,-st2,-end2)
+tot_cells_per_inter <- as.data.frame(t(as.data.frame(t(apply(tot_cells_per_inter, MARGIN = 1, FUN = function(x) length(x[!is.na(x)]) )))))
+tot_cells_per_inter$ID <- row.names(tot_cells_per_inter)
+row.names(tot_cells_per_inter) <- NULL
+colnames(tot_cells_per_inter) <- c("totCells","ID")
+head(tot_cells_per_inter)
+
+
