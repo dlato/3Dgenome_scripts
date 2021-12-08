57ea76c6efc359876fa62b2ddbfb892e2cd04ef3
dlato <daniellalato@gmail.com>
2021-12-08 15:41:39 -0500

    new col for bin of start and end of genes

--- cod_non_cod_anno_per_bin.py
+++ cod_non_cod_anno_per_bin.py
@@ -10,6 +10,7 @@ from gtfparse import read_gtf
 #import Bio
 import pandas as pd
 import numpy as np
+from math import floor
 #from Bio import SeqIO
 #import pprint
 #import gffutils
@@ -18,12 +19,8 @@ import numpy as np
 # FOR DEBUGGING ONLY pdb.set_trace()
 
 gff_file = sys.argv[1]
-bin_size = sys.argv[3]
-print("bin_size")
-print(bin_size)
-print("gff_file")
-print(gff_file)
-
+bin_size = sys.argv[2]
+outfile = sys.argv[3]
 df = read_gtf(gff_file)
 
 #lncRNA_vals <- ["processed_transcript", "lincRNA", "3prime_overlapping_ncrna", "antisense",  "non_coding", "sense_intronic", "sense_overlapping", "TEC", "known_ncrna", "macro_lncRNA",  "bidirectional_promoter_lncrna", "lncRNA"]
@@ -71,215 +68,12 @@ df_genes.loc[df_genes['gene_type'].str.contains("pseudogene"), 'broad_class'] = 
 ##print(df_genes)
 ##df_genes_chr1 = df_genes[df_genes["seqname"] == "1"]
 
-
-
-#examiner = GFFExaminer()
-#in_handle = open(gff_file)
-#print(pprint.pprint(examiner.parent_child_map(in_handle)))
-#in_handle.close()
-#
-#in_handle = open(gff_file)
-#for rec in GFF.parse(in_handle):
-#    print(rec)
-#in_handle.close()
-#faa_filename = sys.argv[2]
-#output_handle = open(faa_filename, "w")
-#output_handle.write("gbk_start\tgbk_end\tgbk_midpoint\tgbk_gene_id\tgbk_locus_tag\tgbk_old_locus_tag\tgbk_strand\n")
-#for gb_record in SeqIO.parse(open(sys.argv[1],"r"), "genbank") :
-#    genes = gb_record.features
-##    print(genes)
-#    for seq_feature in gb_record.features :
-##        if seq_feature.type=="RNA":
-#        m = re.search("[a-z]RNA|CDS",seq_feature.type)
-#        if m:
-##        output_handle.write(str(type(seq_feature.type)))
-##        match_object = re.search(r'*RNA', str(seq_feature.type))
-##        output_handle.write(match_object)
-##        if (seq_feature.type=="CDS" or match_object):
-#            if (seq_feature.qualifiers):
-##                if ('pseudo' in seq_feature.qualifiers):
-##                    continue
-##                #gene is not a pseudogene
-##                else:
-#                end = seq_feature.location.end
-#                start = seq_feature.location.start
-#                start = start + 1
-#                tmp = end + start
-#                midpoint = int(tmp/2)
-#                if seq_feature.strand == 1:
-#               #leading strand = 0
-#                    cod_val = 0
-#                else:
-#               #lagging strand = 1
-#                    cod_val = 1
-#                #dealing with the fact that there might not be a
-#                #gene id present
-#                if ('gene' in seq_feature.qualifiers):
-#                    gene_id = seq_feature.qualifiers['gene'][0]
-#                #gene is is missing
-#                else:
-#                    gene_id = 'NA'
-#                #dealing with the fact that there might not be a
-#                #locus tag present
-#                if ('locus_tag' in seq_feature.qualifiers):
-#                    locus_tag = seq_feature.qualifiers['locus_tag'][0]
-#                #gene is is missing
-#                else:
-#                    locus_tag = 'NA'
-#                #dealing with the fact that there might not be a
-#                #old locus tag present
-#                if ('old_locus_tag' in seq_feature.qualifiers):
-#                    old_locus_tag = seq_feature.qualifiers['old_locus_tag'][0]
-#                #gene is is missing
-#                else:
-#                    old_locus_tag = 'NA'
-#                #print out all the info
-#                output_handle.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (
-#                       start,
-#                       seq_feature.location.end,
-#                       midpoint,
-#                       gene_id,
-#                       locus_tag,
-#                       old_locus_tag,
-#                       cod_val))
-
-#faa_filename = sys.argv[2]
-#output_handle = open(faa_filename, "w")
-#output_handle.write("gbk_start\tgbk_end\tgbk_midpoint\tgbk_gene_id\tgbk_locus_tag\tgbk_old_locus_tag\tgbk_strand\n")
-#for gb_record in SeqIO.parse(open(sys.argv[1],"r"), "genbank") :
-#    genes = gb_record.features
-##    print(genes)
-#    for seq_feature in gb_record.features :
-##        if seq_feature.type=="RNA":
-#        m = re.search("[a-z]RNA|[a-z][a-z]RNA",seq_feature.type)
-#        if m:
-##        output_handle.write(str(type(seq_feature.type)))
-##        match_object = re.search(r'*RNA', str(seq_feature.type))
-##        output_handle.write(match_object)
-##        if (seq_feature.type=="CDS" or match_object):
-#            if (seq_feature.qualifiers):
-#                if ('pseudo' in seq_feature.qualifiers):
-#                    continue
-#                #gene is not a pseudogene
-#                else:
-#                    end = seq_feature.location.end
-#                    start = seq_feature.location.start
-#                    start = start + 1
-#                    tmp = end + start
-#                    midpoint = int(tmp/2)
-#                    if seq_feature.strand == 1:
-#                        #leading strand = 0
-#                        cod_val = 0
-#                    else:
-#                        #lagging strand = 1
-#                        cod_val = 1
-#                    #dealing with the fact that there might not be a
-#                    #gene id present
-#                    if ('gene' in seq_feature.qualifiers):
-#                        gene_id = seq_feature.qualifiers['gene'][0]
-#                    #gene is is missing
-#                    else:
-#                        gene_id = 'NA'
-#                    #dealing with the fact that there might not be a
-#                    #locus tag present
-#                    if ('locus_tag' in seq_feature.qualifiers):
-#                        locus_tag = seq_feature.qualifiers['locus_tag'][0]
-#                    #gene is is missing
-#                    else:
-#                        locus_tag = 'NA'
-#                    #dealing with the fact that there might not be a
-#                    #old locus tag present
-#                    if ('old_locus_tag' in seq_feature.qualifiers):
-#                        old_locus_tag = seq_feature.qualifiers['old_locus_tag'][0]
-#                    #gene is is missing
-#                    else:
-#                        old_locus_tag = 'NA'
-#                #print out all the info
-#                output_handle.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (
-#                       start,
-#                       seq_feature.location.end,
-#                       midpoint,
-#                       gene_id,
-#                       locus_tag,
-#                       old_locus_tag,
-#                       cod_val))
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#output_handle.write("0\t0\t0\t0\t0\t0\t0\n")
-#
-#output_handle.close()
-#
+#add col for what bin the gene is in, listed as START of bin
+def rounddown(x,y):
+    return int(floor(x / int(y))) * int(y)
+df_genes['bin_start'] = df_genes.apply(lambda x: rounddown(x.start, bin_size), 1)
+df_genes['bin_end'] = df_genes.apply(lambda x: rounddown(x.end, bin_size), 1)
+#print(df_genes)
+#print("genes that are between bins")
+#print(df_genes[df_genes['bin_start'] != df_genes['bin_end']])
+df_genes.to_csv(outfile, sep="\t", index=False)
