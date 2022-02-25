51e13790d02a92f710b7f3a27881bfa1fe21a9c6
dlato <daniellalato@gmail.com>
2022-02-25 15:33:31 -0500

    text size UpSet plot

--- cis_interactions_summary_SNPs.R
+++ cis_interactions_summary_SNPs.R
@@ -207,7 +207,7 @@ up <- (ggplot(tidy_zdat, aes(x=cell))
   + geom_bar()
   + geom_text(stat='count', aes(label=after_stat(count)), vjust=-1)
   #+ scale_x_upset(n_intersections = 90)
-  +theme(axis.text.y = element_text(size=10))
+  +theme(axis.text.y = element_text(size=5))
   + scale_x_upset()
   + scale_y_continuous(breaks = NULL, name = "", lim = c(0,20000),)
   + labs(x = "",
